1aa5872035016128a9720f6212313c3e7714d79c
Yeonhyuk Kim <brankein13@gm.gist.ac.kr>
2022-05-24 22:49:05 +0900

    Initial Commit

---
+++ src/main/java/com/yhkim/banksaladviewer/BanksaladViewerApplication.java
@@ -0,0 +1,13 @@
+package com.yhkim.banksaladviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksaladViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanksaladViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhkim/banksaladviewer/BanksaladViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhkim.banksaladviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksaladViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
